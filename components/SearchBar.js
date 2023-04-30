ccf26e76cf6f199a58af413ce5c8597106c8684d
Krothrax <85113500+Krothrax@users.noreply.github.com>
2023-04-29 21:06:22 -0500

    Small Update with scripts and images

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,22 +1,45 @@
 import * as React from 'react';
 import { Searchbar } from 'react-native-paper';
+import { View, Text, FlatList, Image, TouchableOpacity } from 'react-native';
 
 const SearchBar = () => {
   const [searchQuery, setSearchQuery] = React.useState('');
+  const [searchResults, setSearchResults] = React.useState([]);
 
-  const onChangeSearch = query => setSearchQuery(query);
+  const onChangeSearch = async (query) => {
+    setSearchQuery(query);
+    const response = await fetch(`http://example.com/search.php?query=${query}`);
+    const data = await response.json();
+    setSearchResults(data);
+  };
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => { Linking.openURL(item.link) }}>
+      <View style={{ flexDirection: 'row', alignItems: 'center', padding: 10 }}>
+        <Image source={{ uri: item.image }} style={{ width: 50, height: 50, marginRight: 10 }} />
+        <Text>{item.title}</Text>
+      </View>
+    </TouchableOpacity>
+  );
 
   return (
-    <Searchbar
-      placeholder="Search"
-      onChangeText={onChangeSearch}
-      value={searchQuery}
-      icon="magnify"
-      style={{
-        borderRadius: 0,
-        backgroundColor: '#EFEFEF',
-      }}
-    />
+    <View style={{ flex: 1 }}>
+      <Searchbar
+        placeholder="Search"
+        onChangeText={onChangeSearch}
+        value={searchQuery}
+        icon="magnify"
+        style={{
+          borderRadius: 0,
+          backgroundColor: '#EFEFEF',
+        }}
+      />
+      <FlatList
+        data={searchResults}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </View>
   );
 };
 
